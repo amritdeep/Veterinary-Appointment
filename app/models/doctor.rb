8bd1feff141a9381a54ca9c926b23757d1c9d15c
AmritDeep <amritdeepdhungana@hotmail.com>
2016-01-04 14:56:01 -0600

    Valides Doctor model along with Test

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,6 @@
 class Doctor < ActiveRecord::Base
+
+	validates :name, presence: true, length: { maximum: 32 }
+	validates :zip, length: { is: 5 }
+	validates :year_in_practice, length: { in: 1..100 }
 end
--- test/models/doctor_test.rb
+++ test/models/doctor_test.rb
@@ -4,4 +4,34 @@ class DoctorTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Name value is required" do
+  	d = Doctor.new
+  	assert_not d.save
+  end
+
+  test "Name must be no longer than 35 characters" do
+  	d = Doctor.new
+  	d.name = "SusanSusanSusanSusanSusanSusanSusanSusanSusanSusan"
+  	assert_not d.save
+  end
+
+  test "Zip should be at most 5 digits" do
+  	d = Doctor.new
+  	d.zip = 1234567890
+  	assert_not d.save
+  end
+
+  test "Years in Practice  must be value between 1-100" do
+  	d = Doctor.new
+  	d.year_in_practice = 100000
+  	assert_not d.save
+  end
+
+  test "Successful tested" do
+  	d  = Doctor.new(name: "Susan", zip: 12345, year_in_practice: 2)
+  	assert true
+  end
+
+
 end
